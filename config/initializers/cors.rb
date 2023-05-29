a9e019b344f4c5e33c2dcbe05ec36e74265b21a3
shooh25 <koan6210@outlook.jp>
2023-05-29 23:03:26 +0900

    credential

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "https://mygadgets.herokuapp.com"
+    origins "https://gadget-app-frontend.vercel.app"
     # origins "localhost:3000", "https://mygadgets.herokuapp.com"
 
     resource "*",

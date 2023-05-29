08e3a5dc3f1ab6ff66ac4bfc5320d0639c2449b8
shooh25 <koan6210@outlook.jp>
2023-05-29 13:31:29 +0900

    強制

--- db/migrate/20230522025931_create_gadgets.rb
+++ db/migrate/20230522025931_create_gadgets.rb
@@ -3,7 +3,6 @@ class CreateGadgets < ActiveRecord::Migration[7.0]
     create_table :gadgets do |t|
       t.string :userId, :string, null: false, unique: true
       t.string :gadget_category
-
       t.timestamps
     end
   end
--- db/migrate/20230522090502_add_gadget_items_to_gadgets.rb
+++
@@ -1,7 +0,0 @@
-class AddGadgetItemsToGadgets < ActiveRecord::Migration[7.0]
-  def change
-    add_column :gadgets, :gadget_items, :string, array: true, default: [].to_yaml
-    rename_column :gadgets, :userId, :user_id
-    
-  end
-end

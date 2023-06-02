095ca231ca9e01bb983443313957142c98d86478
shooh25 <koan6210@outlook.jp>
2023-06-02 13:23:06 +0900

    Update: URL変更

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "https://mygadgets.vercel.app/"
+    origins "https://mygadgets.vercel.app"
     # origins "localhost:3000", "https://mygadgets.herokuapp.com"
 
     resource "*",

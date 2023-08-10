0d671d4e371d776039a578a1cd0c0f597faddfb3
shooh25 <koan6210@outlook.jp>
2023-08-10 12:44:44 +0900

    fix host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,4 +84,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  
+  # デプロイ後のエラー回避
+  config.hosts << "gadget-app-backend-production.up.railway.app"
 end
